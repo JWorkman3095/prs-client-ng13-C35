642848c44d6e1784d1c69a7ccae96c1e284f5c16
Greg Doud <gpdoud@gmail.com>
2022-04-06 21:03:27 -0400

    finished request review and item

--- src/app/prs/request/request-review-item/request-review-item.component.ts
+++ src/app/prs/request/request-review-item/request-review-item.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SystemService } from '../../system.service';
+import { RequestService } from '../request.service';
+import { Request } from '../request.class';
+import { Requestline } from '../../requestline/requestline.class';
 
 @Component({
   selector: 'app-request-review-item',
@@ -7,9 +12,81 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestReviewItemComponent implements OnInit {
 
-  constructor() { }
+  pageTitle: string = "Request Lines";
+  request!: Request;
+  showVerifyButton: boolean = false;
+  get isAdmin() { 
+    if(!this.sys.isLoggedIn) {
+      return false;
+    } 
+    return this.sys.getLoggedInUser()?.isAdmin; 
+  }
+
+  constructor(
+    private sys: SystemService,
+    private reqsvc: RequestService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  review(): void {
+    this.reqsvc.review(this.request).subscribe({
+      next: (res) => {
+        console.debug("Review:");
+        this.refresh();
+      },
+      error: (err) => console.error(err) 
+    });
+  }
+  edit(reqline: Requestline): void {
+    this.router.navigateByUrl(`/requestlines/edit/${reqline.id}`);
+  }
+  remove(reqline: Requestline): void {
+    this.reqsvc.remove(reqline.id).subscribe({
+      next: (res) => {
+        console.debug("Requestline removed");
+        this.refresh();
+      },
+      error: (err) => console.error(err)
+    });
+  }
+
+  approve(): void {
+    this.reqsvc.approve(this.request).subscribe({
+      next: (res) => {
+        console.debug("Request approved.");
+        this.refresh();
+      },
+      error: (err) => console.error(err)
+    });
+  }
+  reject(): void {
+    this.showVerifyButton = !this.showVerifyButton;
+  }
+  verifyReject(): void {
+    this.showVerifyButton = false;
+    this.reqsvc.reject(this.request).subscribe({
+      next: (res) => {
+        console.debug("Request rejected.");
+        this.refresh();
+      },
+      error: (err) => console.error(err)
+    });
+  }
+
+  refresh(): void {
+    let id = this.route.snapshot.params["id"];
+    this.reqsvc.get(id).subscribe({
+      next: (res) => {
+        console.debug("Request:", res);
+        this.request = res;
+      },
+      error: (err) => console.error(err)
+    });
+  }
 
   ngOnInit(): void {
+    this.refresh();
   }
 
 }
